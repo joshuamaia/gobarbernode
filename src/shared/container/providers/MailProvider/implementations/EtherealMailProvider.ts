9340c4951f21ace4efeaeb3a1d2e2ab3ed5138da
joshuamaia <joshua.curriculo@gmail.com>
2020-05-09 10:28:38 -0300

    Template de emails

--- src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -1,10 +1,17 @@
 import nodemailer, { Transporter } from 'nodemailer';
 import IMailProvider from '../models/IMailProvider';
+import { injectable, inject } from 'tsyringe';
+import ISendMailDTO from '../dtos/ISendMailDTO';
+import IMailTemplateProvider from '../../MailTemplateProvider/models/IMailTemplateProvider';
 
+@injectable()
 export default class EtherealMailProvider implements IMailProvider {
   private client: Transporter;
 
-  constructor() {
+  constructor(
+    @inject('MailTemplateProvider')
+    private mailTemplateProvider: IMailTemplateProvider
+  ) {
     nodemailer.createTestAccount().then(account => {
       const transporter = nodemailer.createTransport({
         host: account.smtp.host,
@@ -20,12 +27,23 @@ export default class EtherealMailProvider implements IMailProvider {
     });
   }
 
-  public async sendMail(to: string, body: string): Promise<void> {
+  public async sendMail({
+    to,
+    from,
+    subject,
+    templateData,
+  }: ISendMailDTO): Promise<void> {
     const mensage = await this.client.sendMail({
-      from: 'Equipe Go Barber <equipe@gobarber.com.br>',
-      to,
+      from: {
+        name: from?.name || 'Equipe Go Barber',
+        address: from?.email || 'equipe@gobarber.com.br',
+      },
+      to: {
+        name: to.name,
+        address: to.email,
+      },
       subject: 'Recuperação de senha',
-      text: body,
+      html: await this.mailTemplateProvider.parse(templateData),
     });
 
     console.log('Message sent: %s', mensage.messageId);
---
+++ src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
@@ -0,0 +1,11 @@
+import IMailTemplateProvider from '../models/IMailTemplateProvider';
+import IParseMailTemplateDTO from '../dtos/IParseMailTemplateDTO';
+
+export default class FakeMailTemplateProvider implements IMailTemplateProvider {
+  public async parse({
+    template,
+    variables,
+  }: IParseMailTemplateDTO): Promise<string> {
+    return template;
+  }
+}
