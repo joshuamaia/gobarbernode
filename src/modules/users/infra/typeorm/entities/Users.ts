5353507d00aacddec890b839cb482b139cd0122b
joshuamaia <joshua.curriculo@gmail.com>
2020-05-16 15:25:43 -0300

    Upload de arquivos para o Amazon S3

--- src/modules/users/infra/typeorm/entities/Users.ts
+++ src/modules/users/infra/typeorm/entities/Users.ts
@@ -7,7 +7,7 @@ import {
 } from 'typeorm';
 
 import { Exclude, Expose } from 'class-transformer';
-
+import uploadConfig from '@config/upload';
 @Entity('users')
 export default class User {
   @PrimaryGeneratedColumn('uuid')
@@ -27,8 +27,17 @@ export default class User {
   updated_at: Date;
   @Expose({ name: 'avatar_url' })
   getAvatarUrl(): string | null {
-    return this.avatar
-      ? `${process.env.APP_API_URL}/files/${this.avatar}`
-      : null;
+    if (!this.avatar) {
+      return null;
+    }
+
+    switch (uploadConfig.driver) {
+      case 'disk':
+        return `${process.env.APP_API_URL}/files/${this.avatar}`;
+      case 's3':
+        return `https://${uploadConfig.config.aws.bucket}.s3.amazonaws.com/${this.avatar}`;
+      default:
+        return null;
+    }
   }
 }
