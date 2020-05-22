ef4ca44735a112dc95f7c22b85c4225e896e625f
joshuamaia <joshua.curriculo@gmail.com>
2020-05-22 16:25:35 -0300

    Clientes dos agendamentos

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -47,6 +47,7 @@ class AppointmentsRepository implements IAppointmentsRepository {
             `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`
         ),
       },
+      relations: ['user'],
     });
 
     return appointments;
